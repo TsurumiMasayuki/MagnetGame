5f1c162d7bd8825e536e666ff235db50b1c2d8bb
makimaki1005 <gumdam1999r@gmail.com>
2020-01-01 17:17:46 +0900

    ObjN追加（仮）

---
+++ MagnetGame/Project1/Source/Actor/Nobject/ObjN.h
@@ -0,0 +1,39 @@
+#pragma once
+#include"Actor/GameObject.h"
+
+class StateManager;
+class Gravity;
+class DetectHelper;
+
+class ObjN:
+	GameObject
+{
+public:
+	ObjN(IGameMediator* nMediator,int dir);
+	~ObjN();
+
+	virtual void start() override;
+	virtual void update() override;
+	virtual void onDestroy() override;
+
+	Gravity * getGravity();
+	GameObject*getMagChange();
+
+	bool isDetectLeft();
+	bool isDetectRight();
+
+private:
+	void initMagChange();
+	void initDetectors();
+private:
+	StateManager*m_nStateManager;
+	Gravity * m_nGravity;
+	GameObject*m_nMagChange;
+
+	DetectHelper*m_nDetectRight;
+	DetectHelper*m_nDetectLeft;
+
+	static const float MOVE_SPEED;
+
+	int direction;
+};
