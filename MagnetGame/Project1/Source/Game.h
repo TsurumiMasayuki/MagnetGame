154197fc142863ab09824b2796d6cc7739c0ef29
内田幸司 <koji0126gogo@yahoo.co.jp>
2020-01-08 12:45:55 +0900

    とりあえずのマップ移動
    ・とりあえずマップ移動をできるように
    ・後ほど滅茶苦茶修正予定

--- MagnetGame/Project1/Source/Game.h
+++ MagnetGame/Project1/Source/Game.h
@@ -6,6 +6,7 @@
 #include "Scene\SceneManager.h"
 #include "Actor\IGameMediator.h"
 #include "Physics\PhysicsWorld.h"
+#include "Math\Vec2.h"
 
 class Tilemap;
 class Player;
@@ -53,5 +54,6 @@ private:
 	Player* m_pPlayer;
   
 	CircleEffect* circleEffect;
+	Vec2 m_CurrentStage;
 };
 
