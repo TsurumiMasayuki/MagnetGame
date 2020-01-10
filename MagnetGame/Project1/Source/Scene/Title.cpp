5131044d7591d54d496425050ed038af3620f431
内田幸司 <koji0126gogo@yahoo.co.jp>
2020-01-10 10:49:48 +0900

    シーンの追加
    GamePlayとTitleのシーンを追加(型のみ)

---
+++ MagnetGame/Project1/Source/Scene/Title.cpp
@@ -0,0 +1,27 @@
+#include "Title.h"
+
+void Title::init()
+{
+}
+
+void Title::update(float deltaTime)
+{
+}
+
+void Title::draw()
+{
+}
+
+void Title::shutdown()
+{
+}
+
+std::string Title::nextScene()
+{
+	return std::string();
+}
+
+bool Title::isEnd()
+{
+	return false;
+}
